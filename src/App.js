65cd817a1af77b7d41acf48a66a625bcd49b9fbf
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-01-24 01:50:16 +0330

    implement regular saving deposit page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DepositType from "./components/DepositType/DepositType";
-
-import "./App.css";
+import RegularSavingsDeposit from "./components/RegularSavingsDeposit/RegularSavingsDeposit";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DepositType />} />
+        <Route path="/saving-deposit" element={<RegularSavingsDeposit />} />
       </Routes>
     </BrowserRouter>
   );
