6aac38db47924522d2a531e4c412f352e88b88b8
mohsen79 <mohsenjalali79.mj@gmail.com>
2025-01-24 18:57:54 +0330

    branches bottom sheet page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DepositType from "./components/DepositType/DepositType";
 
 import "./App.css";
+import Branch from "./components/BottomSheets/Branch/Branch";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DepositType />} />
+        <Route path="/branch" element={<Branch />} />
       </Routes>
     </BrowserRouter>
   );
