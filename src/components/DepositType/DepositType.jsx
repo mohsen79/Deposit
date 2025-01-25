67fd4a72e76da4448a35c2e1295107b8c310681a
MohammadmehdiKhani <mm.y.khani@gmail.com>
2025-01-26 02:09:21 +0330

    refactoring deposite type page and using Ali's checkbox and button

--- src/components/DepositType/DepositType.jsx
+++ src/components/DepositType/DepositType.jsx
@@ -1,53 +1,78 @@
-import BpCheckbox from "../CustomizedCheckbox/CustomizedCheckbox";
 import AbstractDeposit from "./AbstractDeposit/AbstractDeposite";
 import RegularSavingsDepositLogic from "./DepositTypeLogic";
-import { Box, Typography, Button } from "@mui/material";
+import {
+    Box,
+    Typography,
+    Button,
+    FormControlLabel,
+    Checkbox,
+} from "@mui/material";
 
 const DepositType = () => {
-  const { classes } = RegularSavingsDepositLogic();
+    const { classes, styles } = RegularSavingsDepositLogic();
 
-  return (
-    <Box className={classes.container}>
-      <Box className={classes.noActiveDeposit}></Box>
+    return (
+        <Box className={classes.container}>
+            <Box className={classes.noActiveDeposit}></Box>
 
-      <Box className={classes.abstractDepositesContainer}>
-        <AbstractDeposit
-          title={"سپرده قرض الحسنه عادی (دیجیتال)"}
-          description={"حداقل مبلغ افتتاحیه 500،000 ریال ویژه قرعه کشی"}
-          selected={false}
-        ></AbstractDeposit>
-        <AbstractDeposit
-          title={"سپرده قرض الحسنه پس انداز (دیجیتال)"}
-          description={"حداقل مبلغ افتتاحیه 500،000 ریال ویژه تسهیلات"}
-          selected={true}
-        ></AbstractDeposit>
-        <AbstractDeposit
-          title={"سپرده سرمایه گذاری بلند مدت"}
-          description={"حداقل مبلغ افتتاحیه 10،000،000،000 ریال با سود"}
-          selected={false}
-        ></AbstractDeposit>
-        <AbstractDeposit
-          title={"سپرده سرمایه گذاری کوتاه مدت"}
-          description={"حداقل مبلغ افتتاحیه 500،000 ریال با سود"}
-          selected={false}
-        ></AbstractDeposit>
-      </Box>
+            <Box className={classes.abstractDepositesContainer}>
+                <AbstractDeposit
+                    title={"سپرده قرض الحسنه عادی (دیجیتال)"}
+                    description={
+                        "حداقل مبلغ افتتاحیه 500،000 ریال ویژه قرعه کشی"
+                    }
+                    selected={false}
+                ></AbstractDeposit>
+                <AbstractDeposit
+                    title={"سپرده قرض الحسنه پس انداز (دیجیتال)"}
+                    description={
+                        "حداقل مبلغ افتتاحیه 500،000 ریال ویژه تسهیلات"
+                    }
+                    selected={true}
+                ></AbstractDeposit>
+                <AbstractDeposit
+                    title={"سپرده سرمایه گذاری بلند مدت"}
+                    description={
+                        "حداقل مبلغ افتتاحیه 10،000،000،000 ریال با سود"
+                    }
+                    selected={false}
+                ></AbstractDeposit>
+                <AbstractDeposit
+                    title={"سپرده سرمایه گذاری کوتاه مدت"}
+                    description={"حداقل مبلغ افتتاحیه 500،000 ریال با سود"}
+                    selected={false}
+                ></AbstractDeposit>
+            </Box>
 
-      <Box className={classes.stickToBottom}>
-        <Box className={classes.termsAndConditionsAgreement}>
-          <Typography className={classes.description}>
-            <Typography className={classes.emphasizedPart} component="span">
-              قوانین و مقررات{" "}
-            </Typography>
-            را مطالعه کردم و موافقم
-          </Typography>
-          <BpCheckbox></BpCheckbox>
+            <Box className={classes.stickToBottom}>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            id="card-issuance"
+                            disableRipple
+                            size="small"
+                            sx={styles.checkbox}
+                        />
+                    }
+                    label={
+                        <Typography component="span" sx={{ color: "#565656"}}>
+                            <Typography
+                                sx={{ color: "#0564AA", fontWeight: "bold" }}
+                                component="span"
+                            >
+                                قوانین و مقررات{" "}
+                            </Typography>
+                            را مطالعه کردم و موافقم
+                        </Typography>
+                    }
+                    sx={styles.FormControlLabel}
+                />
+                <Box component="section" className={classes.payment}>
+                    <Button className={classes.paymentBtn}>پرداخت</Button>
+                </Box>
+            </Box>
         </Box>
-
-        <Box className={classes.continueBtn}> ادامه </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default DepositType;
--- src/components/DepositType/DepositTypeLogic.js
+++ src/components/DepositType/DepositTypeLogic.js
@@ -1,74 +1,111 @@
 import { createUseStyles } from "react-jss";
 
 const useStyle = createUseStyles({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "end",
-    justifyContent: "center",
-    gap: "20px",
-    padding: "20px 16px",
-    paddingBottom: "50px",
-    boxSizing: "border-box",
-    height: "100vh",
-  },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "end",
+        justifyContent: "center",
+        gap: "20px",
+        padding: "20px 16px",
+        paddingBottom: "50px",
+        boxSizing: "border-box",
+        height: "100vh",
+    },
 
-  abstractDepositesContainer: {
-    marginTop: "20px",
-    display: "flex",
-    flexDirection: "column",
-    gap: "8px",
-    direction: "rtl",
-    width: "100%",
-  },
-  stickToBottom: {
-    display: "flex",
-    flexDirection: "column",
-    gap: "16px",
-    width: "100%",
-    marginTop: "auto",
-  },
-  termsAndConditionsAgreement: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    width: "100%",
-    gap: "7px",
-  },
-  description: {
-    color: "#565656",
-  },
-  emphasizedPart: {
-    fontWeight: "bold",
-    color: "#0564aa",
-  },
-  noActiveDeposit: {
-    width: "100%",
-    height: "136px",
-    border: "1px dashed #989799",
-    borderRadius: "12px",
-  },
-  continueBtn: {
-    backgroundColor: "#0564aa",
-    width: "100%",
-    borderRadius: "12px",
-    height: "42px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    color: "white",
-  },
+    abstractDepositesContainer: {
+        marginTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px",
+        direction: "rtl",
+        width: "100%",
+    },
+    stickToBottom: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+        width: "100%",
+        marginTop: "auto",
+    },
+    termsAndConditionsAgreement: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        width: "100%",
+        gap: "7px",
+    },
+    noActiveDeposit: {
+        width: "100%",
+        height: "136px",
+        border: "1px dashed #989799",
+        borderRadius: "12px",
+    },
+    continueBtn: {
+        backgroundColor: "#0564aa",
+        width: "100%",
+        borderRadius: "12px",
+        height: "42px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+    },
+
+    // payment
+    payment: {
+        height: "42px",
+        width: "100%",
+    },
+    paymentBtn: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        border: "none",
+        borderRadius: "12px",
+        outline: "none",
+        color: "#FFFFFF",
+        backgroundColor: "#0564AA",
+    },
 });
 
+const styles = {
+    FormControlLabel: {
+        display: "block",
+        position: "relative",
+        paddingLeft: "35px",
+        marginRight: "0px",
+        cursor: "pointer",
+        userSelect: "none",
+        "& .MuiTypography-root": {
+            fontSize: "14px",
+            // color: "#565656",
+        },
+    },
+
+    checkbox: {
+        padding: "0",
+        position: "relative",
+        marginLeft: "8px",
+        color: "#565656",
+        "&.Mui-checked": {
+            color: "#00bba7",
+        },
+        "& .MuiSvgIcon-root": {
+            fontSize: 22,
+        },
+    },
+};
+
 const RegularSavingsDepositLogic = () => {
-  const classes = useStyle();
-  /*---------------------- states ------------------- */
+    const classes = useStyle();
+    /*---------------------- states ------------------- */
 
-  /*---------------------- functions ------------------- */
+    /*---------------------- functions ------------------- */
 
-  /*---------------------- data ------------------- */
+    /*---------------------- data ------------------- */
 
-  return { classes };
+    return { classes,styles };
 };
 
 export default RegularSavingsDepositLogic;
